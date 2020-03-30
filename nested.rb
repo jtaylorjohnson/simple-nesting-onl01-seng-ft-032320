d8aa69b36bd0ac5c4be47d450d5ab2b5ae975f31
James Johnson <jtaylorjohnson@gmail.com>
2020-03-30 20:53:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+     string = programmer_hash[:dennis_ritchie][:languages]
+     string
 end
 
 def adding_matz
