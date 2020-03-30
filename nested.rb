bc6f98cdd190742915e9e89907b036a9e44d1043
James Johnson <jtaylorjohnson@gmail.com>
2020-03-30 21:52:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto][:known_for].push("Ruby")
-programmer_hash[:yukihiro_matsumoto][:languages].push("LISP", "C")
+programmer_hash[:yukihiro_matsumoto][:known_for] = ("Ruby")
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+
+# programmer_hash[:yukihiro_matsumoto][:known_for].push("Ruby")
+# programmer_hash[:yukihiro_matsumoto][:languages].push("LISP", "C")
 
 # programmer_hash[:yukihiro_matsumoto][:known_for => "Ruby"]
 # programmer_hash[:yukihiro_matsumoto][:languages => ["LISP", "C"]]
